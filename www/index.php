91ab79cb48f4f3bdeb549cbffc7918a8affa9f0d
Михаил <felix9988@yandex.ru>
2015-05-30 13:58:41 +0300

    e-mail commit

--- www/index.php
+++ www/index.php
@@ -3,8 +3,11 @@ $message = "Текст сообщения";
 $to = "felix9988@mail.ru";
 $from = "felix9988@gmail.ru";
 $subject = "Тема сообщения";
-$subject = "=?utf-8?B?".base64_encode($subject)."?=";
+
 $headers = "From: $from\r\nReply-to: $from\r\nContent-type: text/plain; charset = utf-8\r\n";
+mail($to, $subject, $message, $headers);
+$message = "Сообщение с <b>HTML</b> <i>кодом</i>";
+$headers = "From: $from\r\nReply-to: $from\r\nContent-type: text/html; charset = utf-8\r\n";
 mail($to, $subject, $message, $headers);
 ?>
 <br />
