fd460c4276173c5ceae9ab54a177895dc43c241e
Михаил <felix9988@yandex.ru>
2015-05-31 21:29:53 +0300

    Service rassylki commit

--- www/index.php
+++ www/index.php
@@ -1,11 +1,56 @@
 <?php
+session_start();
+if (isset($_POST["send"])) {
+    $from = $_POST["from"];
+    $to = $_POST["to"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
 
-session_start(); //появляется идентификатор сессии и файл на сервере
-$count = (isset($_SESSION["count"])) ? $_SESSION["count"] : 0;
-$count++;
-$_SESSION["count"] = $count;
-//$_SESSION["count1"] = 15;
-echo "Обновлений страниц: $count раз";
-//через 15 минут сессия удаляется автоматически
-session_destroy(); //очищаем идентификатор сессии
+    $_SESSION["from"] = $from;
+    $_SESSION["to"] = $to;
+    $_SESSION["subject"] = $subject;
+    $_SESSION["message"] = $message;
+}
 ?>
+<html>
+    <head>
+        <title>Cервис рассылки</title>
+    </head>
+    <body>
+        <h1>Отправить почту!</h1>
+        <form name="myform" action="index.php" method="post">
+            <table>
+                <tr>
+                    <td>От кого:</td>
+                    <td>
+                        <input type="text" name="from" value="<?php echo $_SESSION["from"]; ?>" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>Кому:</td>
+                    <td>
+                        <input type="text" name="to" value="<?php echo $_SESSION["to"]; ?>" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>Тема:</td>
+                    <td>
+                        <input type="text" name="subject" value="<?php echo $_SESSION["subject"]; ?>" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>Сообщение:</td>
+                    <td>
+                        <textarea type="message" cols="15" rows="10" ><?php echo $_SESSION["message"]; ?></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <input type="submit" name="send" value="Отправить"/>
+                    </td>
+
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
