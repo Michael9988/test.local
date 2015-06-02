94e9404619e1e9a70753f7223519e86cdc5efe4f
Михаил <felix9988@yandex.ru>
2015-06-02 08:51:37 +0300

    Otpravka pisem2 commit

--- www/index.php
+++ www/index.php
@@ -10,6 +10,35 @@ if (isset($_POST["send"])) {
     $_SESSION["to"] = $to;
     $_SESSION["subject"] = $subject;
     $_SESSION["message"] = $message;
+
+    $error_from = "";
+    $error_to = "";
+    $error_subject = "";
+    $error_message = "";
+
+    if (!preg_match("/^[a-z0-9][a-z0-9\.-_]*[a-z0-9]*@([a-z0-9]+([a-z0-9-]*[a-z0-9]+)*\.)+[a-z]+/i", $from)) {
+        $error_from = "Некорректный e-mail";
+        $error = TRUE;
+    }
+    if (!preg_match("/^[a-z0-9][a-z0-9\.-_]*[a-z0-9]*@([a-z0-9]+([a-z0-9-]*[a-z0-9]+)*\.)+[a-z]+/i", $to)) {
+        $error_to = "Некорректный e-mail";
+        $error = TRUE;
+    }
+    if (strlen($subject) == 0) {
+        $error_subject = "Не написана тема";
+        $error = TRUE;
+    }
+    if (strlen($message) == 0) {
+        $error_message = "Не написано сообщение";
+        $error = TRUE;
+    }
+    if (!$error) {
+        $subject = "=?windows-1251?B?" . base64_encode($subject) . "?=";
+        $headers = "From: $from\r\nReply-to: $from\r\nContent-type: text/plain; charset=windows-1251\r\n";
+        mail($to, $subject, $message, $headers);
+        header("Location: success.php?send=1");
+        exit;
+    }
 }
 ?>
 <html>
@@ -25,11 +54,17 @@ if (isset($_POST["send"])) {
                     <td>
                         <input type="text" name="from" value="<?php echo $_SESSION["from"]; ?>" />
                     </td>
+                    <td>
+                        <span style="color: red;"><?php echo $error_from; ?></span>
+                    </td>
                 </tr>
                 <tr>
                     <td>Кому:</td>
                     <td>
                         <input type="text" name="to" value="<?php echo $_SESSION["to"]; ?>" />
+                    </td>
+                    <td>
+                        <span style="color: red;"><?php echo $error_to; ?></span>
                     </td>
                 </tr>
                 <tr>
@@ -37,15 +72,21 @@ if (isset($_POST["send"])) {
                     <td>
                         <input type="text" name="subject" value="<?php echo $_SESSION["subject"]; ?>" />
                     </td>
+                    <td>
+                        <span style="color: red;"><?php echo $error_subject; ?></span>
+                    </td>
                 </tr>
                 <tr>
                     <td>Сообщение:</td>
                     <td>
-                        <textarea type="message" cols="15" rows="10" ><?php echo $_SESSION["message"]; ?></textarea>
+                        <textarea name="message" cols="22" rows="10" ><?php echo $_SESSION["message"]; ?></textarea>
+                    </td>
+                    <td>
+                        <span style="color: red;"><?php echo $error_message; ?></span>
                     </td>
                 </tr>
                 <tr>
-                    <td colspan="2">
+                    <td colspan="3">
                         <input type="submit" name="send" value="Отправить"/>
                     </td>
 
