7753fb664cf6f6cc8e44d60352e4519739196c4d
Михаил <felix9988@yandex.ru>
2015-07-23 13:18:06 +0600

    UPRAVLENIE_BD

--- www/index.php
+++ www/index.php
@@ -2,5 +2,11 @@
 
 $mysqli = new mysqli("localhost", "root", "", "mybase");
 $mysqli->query("SET NAMES 'utf-8'");
+$mysqli->query("CREATE DATABASE `temp`");
+$mysqli->query("CREATE TABLE `temp`.`cities` (`id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, `title` VARCHAR(255) character set utf8 collate utf8_general_ci NOT NULL) ENGINE=MYISAM CHARACTER SET utf8 COLLATE utf8_general_ci");
+$mysqli->query("ALTER TABLE `temp`.`cities` ADD `utc` TINYINT(2) NOT NULL");
+$mysqli->query("ALTER TABLE `temp`.`cities` DROP `utc`");
+$mysqli->query("DROP TABLE `temp`.`cities`");
+$mysqli->query("DROP DATABASE `temp`");
 $mysqli->close();
 ?>
